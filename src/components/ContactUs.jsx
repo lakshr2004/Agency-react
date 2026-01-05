b3c4fee7aa8d65df92d99a605d3e5a4d75774841
lakshr2004 <lakshr2004@gmail.com>
2026-01-05 13:27:14 +0530

    first commit

---
+++ src/components/ContactUs.jsx
@@ -0,0 +1,108 @@
+import React from 'react'
+import Title from './Title'
+import assets from '../assets/assets'
+import toast from 'react-hot-toast'
+import { motion } from "motion/react"
+
+const ContactUs = () => {
+
+  const onSubmit = async (event) => {
+    event.preventDefault()
+
+    const formData = new FormData(event.target)
+    formData.append("access_key", "f35cc385-ff96-4082-892b-78d88bb47f45")
+
+    try {
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+      })
+
+      const data = await response.json()
+
+      if (data.success) {
+        toast.success('Thank you for your submission')
+        event.target.reset()
+      } else {
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
+  return (
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      transition={{ staggerChildren: 0.2 }}
+      viewport={{ once: true }}
+      id="contact-us"
+      className="flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-20 text-gray-700 dark:text-white"
+    >
+      <Title
+        title="Reach out to us"
+        desc="From strategy to execution, we craft digital solutions that move your business forward."
+      />
+
+      <motion.form
+        initial={{ opacity: 0, y: 30 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.4 }}
+        viewport={{ once: true }}
+        onSubmit={onSubmit} className="grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full">
+
+        <div>
+          <p className="mb-2 text-sm font-medium">Your Name</p>
+          <div className="flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600">
+            <img src={assets.person_icon} alt="" />
+            <input
+              name="name"
+              type="text"
+              placeholder="Enter Your Name"
+              className="w-full p-3 text-sm outline-none bg-transparent"
+              required
+            />
+          </div>
+        </div>
+
+        <div>
+          <p className="mb-2 text-sm font-medium">Email Id</p>
+          <div className="flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600">
+            <img src={assets.email_icon} alt="" />
+            <input
+              name="email"
+              type="email"
+              placeholder="Enter Your Email"
+              className="w-full p-3 text-sm outline-none bg-transparent"
+              required
+            />
+          </div>
+        </div>
+
+        <div className="sm:col-span-2">
+          <p className="mb-2 text-sm font-medium">Message</p>
+          <textarea
+            name="message"
+            rows={8}
+            placeholder="Enter Your Message"
+            className="w-full p-3 text-sm outline-none rounded-lg border border-gray-300 dark:border-gray-600 bg-transparent"
+            required
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="w-max flex items-center gap-2 text-sm bg-primary text-white px-6 py-2 rounded-full hover:scale-105 transition-all"
+        >
+          Submit
+          <img src={assets.arrow_icon} alt="" className="w-4" />
+        </button>
+
+      </motion.form>
+    </motion.div>
+  )
+}
+
+export default ContactUs
